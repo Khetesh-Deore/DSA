8b77133332b6a4279f56b404214e96021d1af520
Khetesh-Deore2246 <kheteshsamadhandeore@gmail.com>
2025-01-12 10:30:55 +0530

    Intial commit

---
+++ Questions/Q1.cpp
@@ -0,0 +1,13 @@
+//Write a program that takes an input of age and print if you are  adult or not 
+#include<bits/stdc++.h>
+using namespace std;
+ int main ()
+ {
+     unsigned int age;
+    cout<<"Enter the Age:";
+    cin>>age;
+    if(age>=18)
+    cout<<"You are Audlt"<<endl;
+    else
+    cout<<"You are not Audlt"<<endl;
+ }
