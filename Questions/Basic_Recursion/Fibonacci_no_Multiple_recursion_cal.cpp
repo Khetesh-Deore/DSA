d1be44751f26721743ebe1e6eb90c7786c1a4760
Khetesh-Deore2246 <kheteshsamadhandeore@gmail.com>
2025-01-25 10:04:14 +0530

    Intial Commit

---
+++ Questions/Basic_Recursion/Fibonacci_no_Multiple_recursion_cal.cpp
@@ -0,0 +1,20 @@
+/* O(2^N)*/
+#include <bits/stdc++.h>
+using namespace std;
+int f(int n)
+{
+    if (n <= 1)
+        return n;
+    int last =f(n-1);
+    int slast=f(n-2);
+    return last+slast;
+}
+int main()
+{
+    int n;
+    cout << "Enter the N: ";
+    cin >> n;
+    cout<<f(n);
+    
+    return 0;
+}
