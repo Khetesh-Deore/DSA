dd78b91e4841fe6c673ea3b8186e8a8c168d5c2e
Khetesh-Deore2246 <kheteshsamadhandeore@gmail.com>
2025-03-24 09:30:12 +0530

    3 SUM PROBLEM (GOOGLE)

---
+++ Questions/Array/Hard/03_3_Sum.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int> nums)
+{
+	sort(nums.begin(), nums.end());
+	vector<vector<int>> ans;
+	int n = nums.size();
+	for (int i = 0; i < n; i++)
+	{
+		if (i > 0 && nums[i] == nums[i - 1])
+			continue;
+		int j = i + 1;
+		int k = n - 1;
+		while (j < k)
+		{
+			int sum = nums[i] + nums[j] + nums[k];
+			if (sum < 0)
+			{
+				j++;
+			}
+			else if (sum > 0)
+			{
+				k--;
+			}
+			else
+			{
+				vector<int> temp = {nums[i], nums[j], nums[k]};
+				ans.push_back(temp);
+				j++;
+				k--;
+				while (j < k && nums[j] == nums[j - 1])
+					j++;
+				while (j < k && nums[k] == nums[k + 1])
+					k--;
+			}
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+	int n;
+	cout << "Enter the Value of N:";
+	cin >> n;
+	cout<<"Enter the Element of array :";
+	vector<int>a(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >>a[i];
+	}
+	vector<vector<int>> ans = threeSum(a);
+	for (auto it : ans)
+	{
+		for (auto ele : it)
+		{
+			cout << ele << " ";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
