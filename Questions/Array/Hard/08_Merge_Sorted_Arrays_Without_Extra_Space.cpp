7044dba41062f1e5775b1558fbca46462cb90894
Khetesh-Deore2246 <kheteshsamadhandeore@gmail.com>
2025-04-02 10:02:20 +0530

    Merge Overlapping intervals

---
+++ Questions/Array/Hard/08_Merge_Sorted_Arrays_Without_Extra_Space.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int f(vector<int> &nums, int n)
+{
+    
+
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements A: ";
+    cin >> n;
+    vector<int> a(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+cout<< f(a, n);
+    
+
+    return 0;
+}
